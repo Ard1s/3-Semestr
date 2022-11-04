41b93cb991a1ace59830e16b2b04d0c5b101fee2
Ard1s <91025158+Ard1s@users.noreply.github.com>
2022-11-04 11:18:09 +0500

    Update simpletimer.cpp

--- Alarm/src/simpletimer.cpp
+++ Alarm/src/simpletimer.cpp
@@ -1,5 +1,5 @@
-#include <limits> // std::numeric_limits
-#include <cmath> // ceil, nearbyint
+#include <limits> 
+#include <cmath> 
 
 #include "simpletimer.h"
 #include "mainwindow.h"
@@ -12,18 +12,18 @@
 #include <QWinTaskbarProgress>
 #endif /* LITTLETIMER_DO_WIN_TASKBAR_PROGRESSBAR */
 
-SimpleTimer::SimpleTimer(const Ui::MainWindow * const ui, MainWindow * const mainwindow) : myTimer(this), myProgressBarUpdateTimer(this) {
+SimpleTimer::SimpleTimer(const Ui::MainWindow * const ui, MainWindow * const mainwindow) : myTimer(this), myProgressBarUpdateTimer(this) 
+{
     running = false;
 
-    myTimer.setSingleShot(true); // timer only fires once
-    connect(&myTimer, &QTimer::timeout, this, &SimpleTimer::stopStuff); // call our "stopStuff" func when timer expires
-    connect(&myTimer, &QTimer::timeout, this, &SimpleTimer::timerFired); // call our "timerFired" func when timer expires
+    myTimer.setSingleShot(true); 
+    connect(&myTimer, &QTimer::timeout, this, &SimpleTimer::stopStuff); 
+    connect(&myTimer, &QTimer::timeout, this, &SimpleTimer::timerFired); 
 
-    myProgressBarUpdateTimer.setSingleShot(false); // fire repeatedly
-    myProgressBarUpdateTimer.setInterval(1000); // fire once per second
-    connect(&myProgressBarUpdateTimer, &QTimer::timeout, this, &SimpleTimer::updateProgressBar); // on every "tick" update the progress bar
+    myProgressBarUpdateTimer.setSingleShot(false); 
+    myProgressBarUpdateTimer.setInterval(1000); 
+    connect(&myProgressBarUpdateTimer, &QTimer::timeout, this, &SimpleTimer::updateProgressBar);   
 
-    // get some pointers to ui elements
     theMainWindow = mainwindow;
     theLineEdit = ui->lineEdit;
     thePushButton = ui->pushButton;
@@ -31,10 +31,9 @@ SimpleTimer::SimpleTimer(const Ui::MainWindow * const ui, MainWindow * const mai
     theProgressBar = ui->progressBar;
 }
 
-// update the progress bar (every second), called by our myProgressBarUpdateTimer, also update the tray icon tooltip (if tray icon is visible)
-void SimpleTimer::updateProgressBar() const {
-    // progress bar value
-    const double percent = 100.0 * myTimer.remainingTime() / myTimer.interval();
+void SimpleTimer::updateProgressBar() const 
+{
+    const double percent = 100.0 * myTimer.remainingTime();
     const int value = static_cast<int>(nearbyint(percent));
     theProgressBar->setValue(value);
 
@@ -42,29 +41,34 @@ void SimpleTimer::updateProgressBar() const {
     wintaskprogress->setValue(value);
 #endif /* LITTLETIMER_DO_WIN_TASKBAR_PROGRESSBAR */
 
-    // label text
-    if(myTimer.remainingTime() > 60000) { // >1min
+    if(myTimer.remainingTime() > 60000) 
+    { // >1min
         myFactorString = "min";
 
-        if(myTimer.remainingTime() > 60000 * 5) { // >5min
-            myRemainingTimeString.setNum(ceil(myTimer.remainingTime() / 60000.), 'f', 0); // for "big minutes" we just use the minute (always round up to full minutes)
-        } else { // <=5min and >1min
-            myRemainingTimeString.setNum(myTimer.remainingTime() / 60000., 'f', 1); // for "small minutes" we use one number after the decimal delimiter (rounds to next 0.1 minute). "showpoint" forces the decimal point.
+        if(myTimer.remainingTime() > 60000 * 5) 
+        { // >5min
+            myRemainingTimeString.setNum(ceil(myTimer.remainingTime() / 60000.), 'f', 0); 
+        } 
+        else 
+        { // <=5min and >1min
+            myRemainingTimeString.setNum(myTimer.remainingTime() / 60000., 'f', 1); 
         }
-    } else {
+    } else 
+    {
         myFactorString = "sec";
-        myRemainingTimeString.setNum(myTimer.remainingTime() / 1000., 'f', 0); // round to full seconds
+        myRemainingTimeString.setNum(myTimer.remainingTime() / 1000., 'f', 0); 
     }
 
     theProgressBar->setFormat(myRemainingTimeString + myFactorString);
 
-    // update tray icon tooltip
-    if(theMainWindow->myTray->isVisible()) {
+    if(theMainWindow->myTray->isVisible()) 
+    {
         theMainWindow->myTray->setToolTip(theMainWindow->windowTitle() + ": " + myRemainingTimeString + myFactorString);
     }
 }
 
-void SimpleTimer::startStuff() {
+void SimpleTimer::startStuff() 
+{
     running = true;
 
     thePushButton->setText(tr("Stop"));
@@ -74,9 +78,8 @@ void SimpleTimer::startStuff() {
 
 #ifdef LITTLETIMER_DO_WIN_TASKBAR_PROGRESSBAR
 
-    // Init the qwintaskbarbutton. From the documentation:
-    // QWidget::windowHandle() returns a valid instance of a QWindow only after the widget has been shown. It is therefore recommended to delay the initialization of the QWinTaskbarButton instances until QWidget::showEvent().
-    if(wintasbarbutton.window() == Q_NULLPTR) {
+    if(wintasbarbutton.window() == Q_NULLPTR) 
+    {
         wintasbarbutton.setWindow(theMainWindow->windowHandle());
         wintaskprogress = wintasbarbutton.progress();
     }
@@ -86,10 +89,11 @@ void SimpleTimer::startStuff() {
 
     myTimer.start();
     myProgressBarUpdateTimer.start();
-    updateProgressBar(); // ProgressBarUpdateTimer does not run until 1sec after we start our stuff, so do a manual update here
+    updateProgressBar(); 
 }
 
-void SimpleTimer::stopStuff() {
+void SimpleTimer::stopStuff() 
+{
     myTimer.stop();
     myProgressBarUpdateTimer.stop();
 
@@ -107,44 +111,47 @@ void SimpleTimer::stopStuff() {
     theProgressBar->setFormat("");
 }
 
-void SimpleTimer::timerFired() const {
-    theMainWindow->myTray->setToolTip(""); // remove tray tooltip
-    theMainWindow->showNormal(); // restore window
+void SimpleTimer::timerFired() const 
+{
+    theMainWindow->myTray->setToolTip(""); 
+    theMainWindow->showNormal(); 
 
-    // actual warning that timer fired for user:
     QMessageBox msg(QMessageBox::Warning, tr("Attention"), tr("Alarm: ") + theMainWindow->windowTitle(), QMessageBox::Ok, thePushButton);
     msg.setWindowModality(Qt::WindowModal);
     msg.exec();
 }
 
-void SimpleTimer::startStopTimer() {
-    // If running: Stop timer. Else: Start timer.
-    if(running) {
+void SimpleTimer::startStopTimer() 
+{
+    if(running) 
+    {
         stopStuff();
-    } else {
-        const QString inputString = theLineEdit->text().replace(',', '.'); // holds the user input
+    } else 
+      {
+        const QString inputString = theLineEdit->text().replace(',', '.');
         const QStringList captures = QRegularExpression("^(\\d{1,2}):(\\d{1,2})$").match(inputString).capturedTexts();
         int newInterval;
 
-        // Check if user input is a time of day or period of time
-        if(captures.length() == 3) {
+        
+        if(captures.length() == 3) 
+       {
             const QTime timeInput = QTime(captures.at(1).toInt(), captures.at(2).toInt());
 
-            if(!timeInput.isValid()) {
+            if(!timeInput.isValid()) 
+            {
                 QMessageBox::warning(thePushButton->parentWidget(), tr("Attention"), tr("Invalid input time (format: HH:MM)."));
                 return;
             }
 
             newInterval = -timeInput.msecsTo(QTime::currentTime());
 
-            // if timeInput is tomorrow (newInterval is negative) we need to do "24hours + newInterval"
-            if(newInterval < 0) {
+            if(newInterval < 0) 
+            {
                 newInterval += 24 * 60 * 60 * 1000;
             }
         } else {
-            unsigned long factor = 0; // factor to convert input value to ms
+            unsigned long factor = 0; 
 
-            // Check which conversion factor user has selected
             switch(static_cast<conversion_factor>(theComboBox->currentIndex())) {
                 case conversion_factor::ms:
                     factor = 1;
@@ -163,12 +170,13 @@ void SimpleTimer::startStopTimer() {
                     break;
             }
 
-            // Convert user input
+            
             bool conversionOkay;
-            const double input = inputString.toDouble(&conversionOkay); // try to convert user input QString to double
+            const double input = inputString.toDouble(&conversionOkay);  
 
-            // Test if conversion was okay (see http://doc.qt.io/qt-5/qstring.html#toDouble) [note: if not ok, then input=0]. QTimer uses int (msec), so make sure we are in the limit of that, also check for negative numbers.
-            if(!conversionOkay || input * factor > std::numeric_limits<int>::max() || input <= 0.) {
+            // Test 
+            if(!conversionOkay || input * factor > std::numeric_limits<int>::max() || input <= 0.) 
+            {
                 QMessageBox::warning(thePushButton->parentWidget(), tr("Attention"), tr("Invalid input! Must be a positive number, which can't be too big (max 596h)."));
                 return;
             }
@@ -176,7 +184,7 @@ void SimpleTimer::startStopTimer() {
             newInterval = static_cast<int>(input * factor);
         }
 
-        myTimer.setInterval(newInterval); // convert input to msec and start the (single shot) timer
+        myTimer.setInterval(newInterval);  
         startStuff();
     }
 }
