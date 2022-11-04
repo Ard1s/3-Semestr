032fbfed6354d49c00aab2db3e753f5478d3df98
Ard1s <91025158+Ard1s@users.noreply.github.com>
2022-11-04 11:21:58 +0500

    Update simpletimer.h

--- Alarm/src/simpletimer.h
+++ Alarm/src/simpletimer.h
@@ -18,30 +18,30 @@ class SimpleTimer : public QObject {
         Q_OBJECT
 
     private:
-        mutable QString myFactorString; // factor string for progressbar label ("min"/"sec")
-        mutable QString myRemainingTimeString; // remaining time string for progressbar label ("6" in "6min")
+        mutable QString myFactorString;  ("min"/"sec")
+        mutable QString myRemainingTimeString; // "6" "6min"
         MainWindow *theMainWindow;
         QPushButton *thePushButton;
         QLineEdit *theLineEdit;
         QComboBox *theComboBox;
         QProgressBar *theProgressBar;
-        QTimer myTimer; // when user requests 6min countdown, this timer deals with that
-        QTimer myProgressBarUpdateTimer; // this timer fires every second and updates the progress bar
+        QTimer myTimer; 
+        QTimer myProgressBarUpdateTimer; 
 #ifdef LITTLETIMER_DO_WIN_TASKBAR_PROGRESSBAR
         QWinTaskbarButton wintasbarbutton;
         QWinTaskbarProgress *wintaskprogress;
 #endif /* LITTLETIMER_DO_WIN_TASKBAR_PROGRESSBAR */
-        void startStuff(); // does stuff when timer is started (e.g. disable button)
+        void startStuff(); 
 
     public:
-        bool running; // if the timer is currently running
-        SimpleTimer(const Ui::MainWindow * const ui, MainWindow * const mainwindow); // constructor
+        bool running; 
+        SimpleTimer(const Ui::MainWindow * const ui, MainWindow * const mainwindow);  
 
     public slots:
-        void stopStuff(); // does stuff when timer is stopped  (e.g. enable button)
-        void timerFired() const; // does stuff to inform user that timer has ended
-        void updateProgressBar() const; // update theProgressBar continuously
-        void startStopTimer(); // start or stop the timer, depending if "running" is true or false
+        void stopStuff(); 
+        void timerFired() const; 
+        void updateProgressBar() const; 
+        void startStopTimer(); 
 };
 
 #endif // SIMPLETIMER_H
