0aa20e7ae441ec81592045b6702ca1b0b6e48fb1
Ard1s <91025158+Ard1s@users.noreply.github.com>
2022-11-04 10:50:19 +0500

    Update main.cpp

--- Alarm/src/main.cpp
+++ Alarm/src/main.cpp
@@ -1,18 +1,18 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
-	Q_INIT_RESOURCE(images); // init resources (needed for static build)
-	QApplication app(argc, argv); // create the QApplication (there can only be a single one)
-	app.setQuitOnLastWindowClosed(false);  // if all windows (timers) are hidden because they are minimized to tray and then one timer is closed, then the app would close without this
+	Q_INIT_RESOURCE(images); 
+	QApplication app(argc, argv); 
+	app.setQuitOnLastWindowClosed(false);  
 
 	MainWindow::theIcon = QIcon(":/hourglass.ico");
 
 	QStringList arguments = app.arguments();
 
-	// create the first default timer window (gets auto deleted)
+	
 	MainWindow *w = new MainWindow();
 	if ( arguments.size() > 1 ) {
-		w->setWindowTitle(arguments.mid(1).join(' ')); // build the window title from command line arguments (except the 0th, i.e, the program name)
+		w->setWindowTitle(arguments.mid(1).join(' '));
 	}
 	w->show();
 
